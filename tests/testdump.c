1870a2fad097de1f111f42d8b5ed46da9ab058ba
Mark Nevill <mark@mark-desktop.(none)>
2007-02-27 20:06:49 +0100

    Added initialization failure check to testdump.c

--- tests/testdump.c
+++ tests/testdump.c
@@ -5,6 +5,7 @@ main(int argc, char* argv[])
 {
 	const char * const * item;
 	xdgHandle handle = xdgAllocHandle();
+	if (!handle) return 1;
 	printf("${XDG_DATA_HOME:-$HOME/.local/share}=%s\n", xdgDataHome(handle));
 	printf("${XDG_CONFIG_HOME:-$HOME/.config}=%s\n", xdgConfigHome(handle));
 	printf("${XDG_DATA_DIRS:-/usr/local/share/:/usr/share/}=");
