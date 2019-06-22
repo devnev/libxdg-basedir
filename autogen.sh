f547ed3d1a6ab358fc5607f1161b2f44244c5379
Davin McCall <davmac@davmac.org>
2019-06-22 15:51:38 +1000

    Don't run configure from autogen.sh.

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+# This script re-generates the "configure" script and associated files,
+# from the configure.ac (autoconf) source.
+
 autoreconf --force --install || exit 1
-./configure "$@" || exit 1
